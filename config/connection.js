c256d17fbf4c811e1671b09aa1a622c9b5704891
Carden Pierre-Vil <pierrevil22@yahoo.com>
2019-05-15 18:33:38 -0400

    NEW

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 var connection; 
-
+//connection to JawDB
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
